60280e3cd7664ae0cb503c9d879366a1b288b644
root <root@localhost.localdomain>
2020-10-05 14:28:09 +0100

    file creation script has modified

--- f.sh
+++ f.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+##creating a dir and moving inside it to create file and delete file
+mkdir test
+cd test
+#creation of the file text
 touch file{1..5}.txt
+#deletion of the file
+rm -rf *
